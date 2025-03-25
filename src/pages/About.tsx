826431c7093dd0784b4906144e2841f14ca31a2d
San Haw <itssarahhaw@gmail.com>
2025-03-26 07:55:10 +0800

    finished layout of hero's page; kulang nalang ang info mismo

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -207,75 +207,54 @@ const About: React.FC = () => {
                       id="asterisk asset"
                     />
                 </div>
+              <div className="w-full rounded-lg bg-transparent aspect-square"></div>
               </div>
             </div>
           </div>
 
                      {/* ---------------------------------------------------------------------- */}
 
-        <div className="grid grid-col-2">
-          {/* image */}
-          <div>
-            <img
-                src="../public/work/temp.jpg"
-                alt="That's a picture of me!"
-                className="w-full h-full 
-                          object-cover
-                          h-[50%]
-            "/>
-          </div>
-          {/* description */}
-          <div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
+        
+            {/* Image Placeholder */}
+            <div className="md:col-span-1">
+              <div className="bg-gray-300 rounded-lg w-full aspect-[3/4]" />
+            </div>
+
+            {/* Text Content */}
+            <div className="md:col-span-2">
+              <h1 className="text-stone-800 
+                            leading-[0.8]
+                            font-[NowCloud]
+                            text-6xl">
+                SARAH<br />HAW
+              </h1>
+
+              <p className="mt-6 leading-relaxed
+                          text-stone-800 
+                            text-lg md:text-base
+              ">
+                Hi, I'm Sarah („• ᴗ •„)♡ An artist and designer from Davao City, Philippines.
+                ･ʚ˚✧˖ With a background in graphic design, ux/ui design, front-end development,
+                project management, social media marketing, and customer service. (*´∇｀*)♡
+                I actively participate in organizations within and outside of school and have
+                led numerous projects since entering college. ⋆✺⋆｡
+              </p>
+            </div>
 
           </div>
-          <div className="ml-4 my-2 2xl:my-4
-                      max-w-full h-32 2xl:h-80 sm:h-28
-                      rounded-lg overflow-hidden 
-                      bg-stone-100 shadow-xl 
-                      flex flex-col sm:flex-row">
-        {/* Left Image */}
-          <div className="w-1/2 overflow-hidden">
-            <img
-              src="../public/work/temp.jpg"
-              alt="That's a picture of me!"
-              className="w-full h-full object-cover"
-            />
-          </div>  
+        
 
-        {/* Right Content */}
-        <div className="w-full sm:w-full align-middle pl-4 pr-4 pt-4 pb-4 flex flex-col">
-          <div>
-            <h2 className="font-[Nowcloud]
-                      text-5xl sm:text-xl
-                      text-gray-900 
-                      mb-4
-            ">Sarah Haw</h2>
-            <p className="
-                    text-gray-800 
-                    text-base sm:text-sm
-                    mt-[-12px] 
-            ">
-              
-            </p>
-          </div>
 
-          {/* Bottom Arrow Icon */}
-          <div className="flex justify-end mt-6">
-          </div>
-        </div>
-      </div>
-        </div>
 
 
                      {/* ---------------------------------------------------------------------- */}
 
-                     <div id="first row of squares" className="grid grid-cols-2 gap-0">
+          <div id="row of squares" className="grid grid-cols-2 gap-0">
             <div></div>
             <div className="col-start-2">
               <div className="grid grid-cols-3 gap-0">
-                <div className="w-full rounded-lg bg-[#E8D0FF] aspect-square"></div>
-                <div className="w-full rounded-lg bg-[#A3CED6] aspect-square"></div>
-                <div className="w-full rounded-lg bg-[transparent aspect-square text-center
+                <div className="col-start-3 w-full rounded-lg bg-[transparent aspect-square text-center
                   flex items-center justify-center overflow-hidden">
                     <img
                       src="/asterisk.svg"
@@ -283,7 +262,13 @@ const About: React.FC = () => {
                       className="w-max h-max object-contain"
                       id="asterisk asset"
                     />
+                
                 </div>
+              </div>
+              <div className="grid grid-cols-3 gap-0">
+                <div className="w-full rounded-lg bg-[#E8D0FF] aspect-square"></div>
+                <div className="w-full rounded-lg bg-[#A3CED6] aspect-square"></div>
+                <div className="w-full rounded-lg bg-[#A3CED6] aspect-square"></div>
               </div>
             </div>
           </div>
