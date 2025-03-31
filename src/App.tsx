3635c42add5eef8d2b4a6119539eebcfea7be359
San Haw <itssarahhaw@gmail.com>
2025-03-31 12:46:01 +0800

    Graphic Design | Cards | adjusted responziveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Navbar from './components/Navbar';
 const App = () => {
   return (
-    <div className='bg-stone-100 w-full overflow-hidden'>
+    <div className='bg-stone-100 w-full overflow-hidden max-w-full'>
       <Head>
       <></>
     </Head>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,7 @@ const Card: React.FC<CardProps> = ({ title, description, imageUrl, onCardClick, 
   return (
     <div
       className="rounded-lg overflow-hidden shadow-md bg-white 
-                  w-90 2xl:w-100 xl:w-100 lg:w-96 md:w-75 sm:w-70 xs:w-80
+                  w-90 2xl:w-120 xl:w-100 lg:w-80 md:w-75 sm:w-70 xs:w-80
                   cursor-pointer transform transition-all duration-300 
                   ease-in-out hover:scale-105 active:bg-stone-200 hover:shadow-lg"
       onClick={() => onCardClick(link)} // Call the onCardClick with the link passed as prop
