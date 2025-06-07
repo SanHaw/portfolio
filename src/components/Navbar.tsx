e8adc51c8e721882c4b0ee583beb638743cbc458
San Haw <itssarahhaw@gmail.com>
2025-06-07 19:49:20 +0800

    Projects || added new projects page, restructured routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,15 +9,14 @@ import '../index.css';
 import Page404 from '../pages/404';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
-import GraphicDesign from '../pages/GraphicDesign';
+import Projects from '../pages/Projects';
 import Resume from '../pages/Resume';
-import UXUI from '../pages/UXUI';
 
 const Navbar: React.FC = () => {
   return (
     <Router>
       <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between bg-stone-100 text-stone-800 mb-6 px-16 py-4 pt-8 
-                    position-fixed top-0 z-index-10000
+                    position-fixed z-index-10000
       ">
         
         <div className="flex items-center">
@@ -28,8 +27,9 @@ const Navbar: React.FC = () => {
 
         <nav className="flex space-x-6 font-[Roboto] text-lg">
           <Link to="/" className="">About Me</Link>
-          <Link to="/graphic-design" className="">Graphic Design</Link>
-          <Link to="/uxui" className="">UX/UI</Link>
+          <Link to="/projects" className="">Projects</Link>
+          {/* <Link to="/graphic-design" className="">Graphic Design</Link>
+          <Link to="/uxui" className="">UX/UI</Link> */}
           <Link to="/contact" className="">Contact</Link>
           <Link to="/resume" className="">Resume</Link>
         </nav>
@@ -38,8 +38,9 @@ const Navbar: React.FC = () => {
       {/* Routes */}
       <Routes>
         <Route path="/" element={<About />} />
-        <Route path="/graphic-design" element={<GraphicDesign />} />
-        <Route path="/uxui" element={<UXUI />} />
+        {/* <Route path="/graphic-design" element={<GraphicDesign />} /> */}
+        <Route path="/projects" element={<Projects />} />
+        {/* <Route path="/uxui" element={<UXUI />} /> */}
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/404" element={<Page404 />} />
